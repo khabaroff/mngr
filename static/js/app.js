3ec20652b8f585c8f6b735ee0c3e9570c18212d1
Sergey Khabaroff <khabaroff@gmail.com>
2015-08-31 01:05:44 +0300

    2

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,122 @@
-var VK = {
-    Share: {
-        count: function (value, count) {
-            $('#vkontakte_count').html(count);
-        }
-    }
-};
-
-
-function numbers(page_url) {
-
-
-    $.getJSON('http://vkontakte.ru/share.php?act=count&index=1&url=' + encodeURI(page_url) + '&callback=?', function (response) {
-    });
-
-
-    $.getJSON('http://graph.facebook.com/' + encodeURI(page_url), function (response) {
-        if (response.shares !== undefined) {
-            b = response.shares;
-            $('#fb_count').html(response.shares);
-
-        } else {
-            $('#fb_count').html(0);
-        }
-    });
-
-    $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=' + encodeURI(page_url) + '&callback=?', function (response) {
-        if (response.count !== undefined) {
-
-            $('#tw_count').html(response.count);
-        }
-
-        else {
-            $('#tw_count').html(0);
-
-        }
-
-    });
-
-
-};
-
-
-$(function () {
-    $("#shares").click(function () {
 
 
 
-        $(".row").addClass("valign-topper");
-        $(".counters").addClass("counters-topper");
 
-        numbers($("#url").val());
+ $(function () {
+     $("#shares").click(function () {
 
+         var mainLink = $("#url").val();
 
+         if  (mainLink.indexOf("://")) {
 
-        console.log($('#fb_count').html());
-
-        $('#datafb').animateNumber({number: b});
-
-
-    });
+                 $(".md-trigger" ).trigger( "click" );
+         }
 
 
-});
+         //=================
+
+
+         (function () {
+             window.VK = {
+                 Share: {
+                     queue: [],
+                     push: function (url, defer) {
+                         VK.Share.queue.push({url: url, defer: defer})
+                         VK.Share.request()
+                     },
+                     request: function () {
+                         if (VK.Share.progress) {
+                             return
+                         }
+
+                         var job = VK.Share.queue.shift()
+
+                         if (!job) {
+                             return
+                         }
+
+                         VK.Share.progress = true
+
+                         VK.Share.count = VK.Share._count.bind(this, job.defer)
+
+                         $.getScript('http://vkontakte.ru/share.php?act=count&index=1&url=' + encodeURI(job.url), function () {})
+                     },
+                     _count: function (defer, i, count) {
+                         defer && defer.resolve && defer.resolve(count)
+
+                         VK.Share.progress = false
+                         VK.Share.request()
+                     }
+                 }
+             }
+
+             function numbers(page_url) {
+                 var resp = {
+                     vk: $.Deferred(),
+                     twitter: $.Deferred(),
+                     fb: $.Deferred()
+                 }
+
+                 VK.Share.push(page_url, resp.vk)
+
+                 $.getJSON('http://graph.facebook.com/' + encodeURI(page_url), function (response) {
+                     resp.fb.resolve(response && response.shares || 0)
+                 });
+
+                 $.getJSON('http://urls.api.twitter.com/1/urls/count.json?url=' + encodeURI(page_url) + '&callback=?', function (response) {
+                     resp.twitter.resolve(response && response.count || 0)
+                 });
+
+                 return resp
+             };
+
+
+
+             var resp = numbers(mainLink)
+
+             $.when(resp.vk, resp.fb, resp.twitter).done(function (vkCount, fbCount, twCount) {
+
+
+                 $(".row").addClass("valign-topper");
+                 $(".counters").addClass("counters-topper");
+
+                 $('#datafb').animateNumber({number: fbCount});
+                 $('#datavk').animateNumber({number: vkCount});
+                 $('#datatw').animateNumber({number: twCount});
+                 $('#dataall').animateNumber({number: fbCount+vkCount+twCount});
+
+
+                 $("#linkfb").attr("href", "https://www.facebook.com/search/str/"+encodeURIComponent(mainLink)+"/keywords_top");
+                 $("#linkvk").attr("href", "https://vk.com/search?c%5Bq%5D="+encodeURIComponent(mainLink)+"&c%5Bsection%5D=auto");
+                 $("#linktw").attr("href", "https://twitter.com/search?q="+encodeURIComponent(mainLink.substring(mainLink.indexOf("//")+2))+"&src=typd");
+
+                 //console.log(
+                 //    {
+                 //        vk: vkCount,
+                 //        fb: fbCount,
+                 //        tw: twCount
+                 //    }
+                 //)
+             })
+
+         })();
+
+         //=================
+
+
+
+
+
+
+
+
+
+
+
+
+
+     });
+
+
+ });
 
